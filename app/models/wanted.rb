8ccc8a224a765199a803076332d57c151b481f84
mikosu3 <mikosu3@gmail.com>
2016-02-27 01:59:27 +0900

    履歴登録処理を追加

--- app/models/wanted.rb
+++ app/models/wanted.rb
@@ -1,8 +1,23 @@
 class Wanted < ActiveRecord::Base
   has_one :credit
+  after_save :add_history
 
   # 検索許可するパラメータ
   def self.ransackable_attributes auth_object = nil
     %w(file_name folder_name extension)
   end
+
+  private
+
+    # 履歴追加
+    def add_history
+      history = History.new
+
+      history.link = '/credits/new/' + self.id.to_s
+      history.item = 'Wanted'
+      history.item_name = self.file_name
+      history.is_create = true
+
+      history.save
+    end
 end
