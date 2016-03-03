9cb5093ea25dcc079be8e6656b9ffc0e670d5219
mikosu3 <mikosu3@gmail.com>
2016-03-04 02:10:35 +0900

    ファイル出力パス修正

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 worker_processes 4
 
-listen '/tmp/unicorn.sock'
-pid '/tmp/unicorn.pid'
+listen 'tmp/unicorn.sock'
+pid 'tmp/unicorn.pid'
 
 stderr_path File.expand_path('log/unicorn.log')
 stdout_path File.expand_path('log/unicorn.log')
