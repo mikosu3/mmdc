52e3fda828a2a611ad5233bd4f0f1f69ae4a62ca
mikosu3 <mikosu3@gmail.com>
2016-03-04 02:52:10 +0900

    本番設定

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,7 +1,11 @@
 # -*- coding: utf-8 -*-
+working_directory "/var/www/mmdc"
 worker_processes 4
 
-listen '/var/www/mmdc/tmp/unicorn.sock'
+listen "/var/www/mmdc/tmp/unicorn.sock", :backlog => 64
+listen 5000, :tcp_nopush => true
+#listen '/var/www/mmdc/tmp/unicorn.sock'
+
 pid '/var/www/mmdc/tmp/unicorn.pid'
 
 stderr_path File.expand_path('log/unicorn.log')
