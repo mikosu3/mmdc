ee3c16bb6897180b552867edfa43d1cb4b35001c
mikosu3 <mikosu3@gmail.com>
2016-03-24 17:20:59 +0900

    ログイン時ユーザー情報を更新

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,17 @@ class SessionsController < ApplicationController
 
   def callback
     auth = request.env['omniauth.auth']
-    user = User.find_by(provider: auth["provider"], uid: auth["uid"]) || User.create_with_omniauth(auth)
+    user = User.find_by(provider: auth["provider"], uid: auth["uid"])
+
+    if user.nil?
+      user = User.create_with_omniauth(auth)
+    else
+      #ユーザー情報更新
+      user.screen_name = auth['info']['nickname']
+      user.name = auth['info']['name']
+      user.save
+    end
+
     session[:user_id] = user.id
     redirect_to root_path
   end
