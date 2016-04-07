2d7708d3623938319a435aa78f3db63441fb4ea6
mikosu3 <mikosu3@gmail.com>
2016-04-07 23:13:09 +0900

    貢献度ランキングTOP3を表示する

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
   def index
     @side_infos = Info.order(id: :desc).first(5)
+    @ranks = RankingService.getRanks(3)
 
     respond_to do |format|
       format.html
